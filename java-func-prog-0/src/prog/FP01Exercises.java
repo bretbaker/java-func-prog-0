24502f46e345d931f18ec2da37221eba98b3e1f4
Bret Baker <bretbaker808@gmail.com>
2020-11-11 14:55:13 -0500

    nc

---
+++ java-func-prog-0/src/prog/FP01Exercises.java
@@ -0,0 +1,42 @@
+package prog;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FP01Exercises {
+
+	public static void main(String[] args) {
+		List<Integer> numbers = Arrays.asList(12, 9, 13, 4, 6, 2, 4, 12, 15);
+		printCubesOfOddNumbersInListFunctional(numbers);
+//		printOddNumbersInListFunctional(numbers);
+		List<String> courses = Arrays.asList("Spring", "Spring Boot", "API", "Microservices", "AWS", "PCF", "Azure",
+				"Docker", "Kubernetes");
+
+		// courses.stream()
+		// .forEach(System.out::println);
+
+		// courses.stream()
+		// .filter(course -> course.contains("Spring"))
+		// .forEach(System.out::println);
+
+		// courses.stream()
+		// .filter(course -> course.length() >= 4)
+		// .forEach(System.out::println);
+
+		courses.stream().map(course -> course + " " + course.length()).forEach(System.out::println);
+	}
+
+	private static void printOddNumbersInListFunctional(List<Integer> numbers) {
+		numbers.stream() // Convert to Stream
+				.filter(number -> number % 2 != 0) // Lamdba Expression
+				.forEach(System.out::println);// Method Reference
+	}
+
+	private static void printCubesOfOddNumbersInListFunctional(List<Integer> numbers) {
+		numbers.stream() // Convert to Stream
+				.filter(number -> number % 2 != 0) // Lamdba Expression
+				.map(number -> number * number * number).forEach(System.out::println);// Method Reference
+	}
+
+}
---
+++ java-func-prog-0/src/prog/FP02Structured.java
@@ -0,0 +1,28 @@
+package prog;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class FP02Structured {
+
+	public static void main(String[] args) {
+
+		List<Integer> numbers = Arrays.asList(12, 9, 13, 4, 6, 2, 4, 12, 15);
+		
+		int sum = addListStructured(numbers);
+		
+		System.out.println(sum);
+		
+
+	}
+
+	private static int addListStructured(List<Integer> numbers) {
+		//how to loop?
+		//how to store the sum?
+		int sum = 0;
+		for(int number:numbers) {
+			sum += number;
+		}
+		return sum;
+	}
+}
